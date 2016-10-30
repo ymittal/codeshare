1f00c34a9c9f61772eacf8c9bb0733fde3e11369
Yash Mittal <ym012@bucknell.edu>
2016-10-30 03:30:46 -0400

    Fixed admin page routing

--- django_project/urls.py
+++ django_project/urls.py
@@ -20,8 +20,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'', include('codeshare.urls', namespace='codeshare')),
     url(r'^admin/', admin.site.urls),
+    url(r'^', include('codeshare.urls', namespace='codeshare')),
 ]
 
 if settings.DEBUG:
