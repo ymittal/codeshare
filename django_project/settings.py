4e0a07346cc00a0203e02d129d01d2132729c90f
Yash Mittal <yashmittal2009@gmail.com>
2016-12-21 10:04:03 +0530

    Fixed CONTEXT_PROCESSORS in settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -68,13 +68,6 @@ TEMPLATES = [
     },
 ]
 
-TEMPLATE_CONTEXT_PROCESSORS = [
-    'django.template.context_processors.debug',
-    'django.template.context_processors.request',
-    'django.contrib.auth.context_processors.auth',
-    'django.contrib.messages.context_processors.messages',
-]
-
 WSGI_APPLICATION = 'django_project.wsgi.application'
 
 # Database
